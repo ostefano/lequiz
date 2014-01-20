ef4ce9cbef1f4e927851eb269e0829c6afe20bb7
Stefano Ortolani <ostefano@gmail.com>
2014-01-20 22:29:57 +0000

    fixed require_once in cli.php

--- cli.php
+++ cli.php
@@ -1,7 +1,7 @@
 <?php
 
 	require_once('config.inc.php');
-	require_once('phpexcel/PHPExcel.php');
+	require_once('phpexcel/Classes/PHPExcel.php');
 	require_once('php-spreadsheetreader/SpreadsheetReaderFactory.php');
 
 	// TEST where we read a JSON and print all its fields
